a79c27f9232bd9e53260354a018527d878b59765
liyongheng <386701882@qq.com>
2017-01-13 01:19:52 +0800

    ArrayAccess实现

---
+++ ArrayAccess/ArrayAccess.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * implements ArrayAccess
+ * @author liyongheng
+ */
+class arrayAcs implements ArrayAccess
+{
+    protected $attributes = [];
+    
+    public function offsetGet($offset)
+    {
+        return $this->attributes[$offset];
+    }
+    
+    public function offsetSet($offset, $value)
+    {
+        $this->attributes[$offset] = $value;
+    }
+    
+    public function offsetExists($offset)
+    {
+        return isset($this->$attributes[$offset]);
+    }
+    
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
+    }
+}
+
+$aa = new arrayAcs();
+$aa['name'] = 'liyongheng';
+echo '<pre>';
+print_r($aa);
+print_r($aa['name']);
